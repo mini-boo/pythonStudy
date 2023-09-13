cb700980d30dc33d6e193eb0423ea0519c6cea6f
Hyunsoo Kim <sgn06221@gmail.com>
2023-09-13 18:14:04 +0900

    [feat] step03_숫자 추출, 카드 역배치

--- PythonAlgorithm/백준/AA.py
+++ PythonAlgorithm/백준/AA.py
@@ -1,11 +1,10 @@
-Number = int(input())
-sort_list = []
+n=int(input())
 
-for i in range(Number):
-    sort_list.add(int(input()))
-
-sort_list = list(sort_list)
-sort_list.sort()
-
-for j in range(len(sort_list)):
-    print(sort_list[j])
+for i in range(n):
+    a=int(input())
+    size=len(a)
+    for j in range (size//2):
+        if a[j]!=a[-j-1]:
+            print(0)
+        else:
+            print(1)
